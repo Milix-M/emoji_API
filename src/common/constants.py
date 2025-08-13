9f08eab6bb3fec6374e981a0a77160943f63767a
Milix-M <70957923+Milix-M@users.noreply.github.com>
2025-08-13 22:01:54 +0900

    フォントに関する定数を分離し、フォント管理定数に書体に関する情報を追加

---
+++ src/common/constants.py
@@ -0,0 +1,25 @@
+class Fonts:
+    """フォントに関する定数を定義するクラス"""
+
+    FONT_NAME_PATH_MAPPING = {
+        "M+ 1p black": {
+            "path": "/fonts/MPLUS1p-Black.ttf",
+            "typeface": "Gothic",
+        },
+        "Rounded M+ 1p black": {
+            "path": "/fonts/rounded-mplus-1p-black.ttf",
+            "typeface": "Rounded Gothic",
+        },
+        "Noto Sans JP": {
+            "path": "/fonts/NotoSansJP-Black.ttf",
+            "typeface": "Gothic",
+        },
+        "Sawarabi Mincho": {
+            "path": "/fonts/SawarabiMincho-Regular.ttf",
+            "typeface": "Mincho",
+        },
+        "YuseiMagic": {
+            "path": "/fonts/YuseiMagic-Regular.ttf",
+            "typeface": "Rounded Gothic",
+        },
+    }
